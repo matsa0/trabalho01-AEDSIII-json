3eaea7b4fe6c0b38002cde8fd4829d97003bad82
Matheus Azevedo <matheus.azevedo@aluno.ufop.edu.br>
2023-07-04 19:18:42 -0300

    Passando o código para funções no weighted_graph.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 """ import requests"""
-import json
 from weighted_graph import WeightedGraph
 
 g = WeightedGraph()
@@ -8,49 +7,24 @@ file_name = input("Informe o arquivo de votações > ")
 print("Processando...")
 
 #1 - Leitura do json
-with open(file_name, 'r', encoding='utf-8') as file:
-    content = json.load(file)
+content = g.read_json_file(file_name)
 
 #2 - Adicionando arestas e peso(relação de proximidade)
-votes = {}
-for item in content['dados']:
-    name = item['deputado_']['nome']
-    vote_id = item['idVotacao']
-    vote = item['voto']
-    g.add_node(name)
-
-    for key, value in votes.items():
-        if value['idVotacao'] == vote_id and value['voto'] == vote:
-            if g.there_is_edge(name, key):
-                g.increment_edge_weight(name, key)
-            else:
-                g.add_edge(name, key, 1)
-
-    votes[name] = {'idVotacao': vote_id, 'voto': vote}
+g.add_votes_edge(content)
 
 #3 - Escrevendo no arquivo "deputados.txt" 
-with open("deputados.txt", 'w', encoding='utf-8') as file:
-    file.write(f"{g.node_count} {g.edge_count}\n")
-    for node1 in g.adj_list:
-        for node2, weight in g.adj_list[node1].items():
-            file.write(f"{node1} {node2} {weight}\n")
+g.file_write_graph("deputados.txt")
 
 #4 - Verificando quantas votações cada nó paricipou
-votes_per_node = {}
-for item in content['dados']:
-    name = item['deputado_']['nome']
-    if name not in votes_per_node:
-        votes_per_node[name] = 0
-    votes_per_node[name] += 1
+votes_per_node = g.verify_votes(content)
 
 #5 - Escrevendo no arquivo votos.txt
-with open("votos.txt", 'w', encoding='utf-8') as file:
-    for node in g.adj_list:
-        votes = votes_per_node.get(node, 0)
-        file.write(f"{node} {votes}\n")
+g.file_write_votes("votos.txt", votes_per_node)
 
 print("O Grafo foi escrito nos arquivos: ")
 print("\t-deputados.txt\n\t-votos.txt")
+
+
 
 
 """ url = 'https://dadosabertos.camara.leg.br/api/v2/deputados?ordem=ASC&ordenarPor=nome'
--- weighted_graph.py
+++ weighted_graph.py
@@ -42,14 +42,54 @@ class WeightedGraph:
         self.edge_count -= 1
     
     def read_json_file(self, file_name):
-        with open(file_name, 'r') as file:
+        with open(file_name, 'r', encoding='utf-8') as file:
             content = json.load(file)
         return content
 
-    def write_in_file(self, file_name, infos):
-        with open(file_name, 'w') as file:
-            for info in infos:
-                file.write(str(info) + '\n')
+    def add_votes_edge(self, content):
+        votes = {}
+        for item in content['dados']:
+            name = item['deputado_']['nome']
+            vote_id = item['idVotacao']
+            vote = item['voto']
+            self.add_node(name)
+    
+            for key, value in votes.items():
+                if value['idVotacao'] == vote_id and value['voto'] == vote:
+                    if self.there_is_edge(name, key):
+                        self.increment_edge_weight(name, key)
+                    else:
+                        self.add_edge(name, key, 1)
+    
+            votes[name] = {'idVotacao': vote_id, 'voto': vote}
+
+    def file_write(self, file_name, content):
+        with open(file_name, 'w', encoding='utf-8') as file:
+            file.write(content)
+    
+    def file_write_graph(self, file_name):
+        content = f"{self.node_count} {self.edge_count}\n"
+        for node in self.adj_list:
+            for node2, weight in self.adj_list[node].items():
+                content += f"{node} {node2} {weight}\n"
+        self.file_write(file_name, content)
+
+        
+    def verify_votes(self, content):
+        votes_per_node = {}
+        for item in content['dados']:
+            name = item['deputado_']['nome']
+            if name not in votes_per_node:
+                votes_per_node[name] = 0
+            votes_per_node[name] += 1
+        return votes_per_node
+
+    def file_write_votes(self, file_name, votes_per_node):
+        content = ""
+        for node in self.adj_list:
+            votes = votes_per_node.get(node, 0)
+            content += f"{node} {votes}\n"
+        self.file_write(file_name, content)
 
     def __str__(self):
         output = ""
@@ -60,17 +100,4 @@ class WeightedGraph:
                 output += f"{neighbor} {weight}, "
             output = output.rstrip(", ") + "\n"
         return output
-'''        
-    def read_file(self, file_name):
-        with open(file_name, 'r') as file: #garante que o arquivo abra e seja fechado corretamente
-            i = 0
-            for line in file:   
-                i += 1
-                if i == 1:
-                    continue
-            content = line.strip().split(" ")
-            u = content[0]
-            v = content[1]
-            w = int(content[2])
-            self.add_edge(u, v, w)
-'''
+
